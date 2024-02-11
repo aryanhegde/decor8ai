a190b14323e20d6ddd27c4ab34b1dccbbe2ca059
aryanhegde <hegdearyan000@gmail.com>
2024-02-11 23:46:46 +0530

    random 0

--- components/NavCreate.tsx
+++ components/NavCreate.tsx
@@ -1,7 +1,21 @@
+import appwriteService from "@/appwrite/config";
 import React from "react";
 
 const NavCreate = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="flex items-center justify-between">
+      <div className="p-4">
+        <h2 className="text-2xl font-semibold">Decor8AI</h2>
+      </div>
+      <div className="flex  gap-x-4 mx-8">
+        <p className="font-medium text-slate-700">Credits</p>
+        <h1 className="font-bold text-primary underline underline-offset-1">
+          {" "}
+          Name Here
+        </h1>
+      </div>
+    </nav>
+  );
 };
 
 export default NavCreate;
